310908a243e107495048e72c1025344992ea4e3f
Arthur Couto <arthurscouto@outlook.com>
2022-08-29 00:51:41 -0700

    Adiciona Collider Render, adapta codigo para integrar ao resto do programa

--- src/Collider.cpp
+++ src/Collider.cpp
@@ -1,5 +1,14 @@
 #include "Collider.h"
 
+//#define DEBUG
+#ifdef DEBUG
+#include "Camera.h"
+#include "Game.h"
+
+#include <SDL2/SDL.h>
+#endif // DEBUG
+#define PI 3.1415926
+
 Collider::Collider(GameObject& associated, Vec2 scale, Vec2 offset) : Component(associated){
 	box=associated.box;
 	this->scale=scale;
@@ -15,8 +24,27 @@ void Collider::Update(float dt){
 	return;
 }
 
-void Collider::Render(){
-	return;
+void Collider::Render() {
+#ifdef DEBUG
+	Vec2 center( box.Center() );
+	SDL_Point points[5];
+
+	Vec2 point = (Vec2(box.x, box.y).Sub(center)).Rotate( associated.angleDeg/(180/PI) ).Add(center.Sub(Camera::pos));
+	points[0] = {(int)point.x, (int)point.y};
+	points[4] = {(int)point.x, (int)point.y};
+	
+	point = (Vec2(box.x + box.w, box.y).Sub(center)).Rotate( associated.angleDeg/(180/PI) ).Add(center.Sub(Camera::pos));
+	points[1] = {(int)point.x, (int)point.y};
+	
+	point = (Vec2(box.x + box.w, box.y + box.h).Sub(center)).Rotate( associated.angleDeg/(180/PI) ).Add(center.Sub(Camera::pos));
+	points[2] = {(int)point.x, (int)point.y};
+	
+	point = (Vec2(box.x, box.y + box.h).Sub(center)).Rotate( associated.angleDeg/(180/PI) ).Add(center.Sub(Camera::pos));
+	points[3] = {(int)point.x, (int)point.y};
+
+	SDL_SetRenderDrawColor(Game::GetInstance().GetRenderer(), 255, 0, 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderDrawLines(Game::GetInstance().GetRenderer(), points, 5);
+#endif // DEBUG
 }
 
 bool Collider::Is(std::string type){
